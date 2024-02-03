1e583ec5c6a907f3c9377ab794f1aeaf695450f7
hemant lodha <hemantlodha1000@gmail.com>
2024-02-03 06:52:41 +0530

    fifth Commit(some css part)

--- ui-ux/src/main.jsx
+++ ui-ux/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Assignment from './assignment.jsx'
+import Student from './student.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Assignment />
+    <Student />
   </React.StrictMode>,
 )
--- ui-ux/src/student.jsx
+++ ui-ux/src/student.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 const Student = () => {
   return (
     <div>
-      
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Student</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="MyTasks" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Task1</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Task2
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Task3</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </div>
   )
 }
