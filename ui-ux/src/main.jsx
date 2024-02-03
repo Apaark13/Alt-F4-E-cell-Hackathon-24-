a97083f3754c6ea40b10b70734f77e0327c0c59e
aamiir23 <iammdaamiralam@gmail.com>
2024-02-04 01:07:10 +0530

    ui improvement and bug fixtures

--- ui-ux/src/main.jsx
+++ ui-ux/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Assignment from './assignment.jsx'
 import Student from './student.jsx'
-
+import Assgnment_mini from './assgnment_mini.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
